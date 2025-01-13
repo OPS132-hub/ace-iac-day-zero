f74724e9d56942c2b3b6b0b05fd272e2f5a7082e
OPS132-hub <ahmedection@duck.com>
2025-01-13 10:43:19 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "ahmedection-IAC"
+    organization = "ahmedection-IAC"
     workspaces {
       name = "ace-iac-day-zero"
     }
